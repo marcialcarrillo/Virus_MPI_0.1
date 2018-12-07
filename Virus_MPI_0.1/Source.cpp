c823b501dbd38126a036d029ad4107a65e5cfdc4
Marcial Carrillo <marcialcarrillov@gmail.com>
2018-12-07 13:26:28 -0600

    Add project files.

---
+++ Virus_MPI_0.1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int input_people_quantity;
+double input_chance_infection;
+double input_chance_recover;
+int input_infection_duration;
+double input_perc_initial_infected;
+int input_map_size;
+int input_tick_quantity;
+
+int main(int argc, char ** argv)
+{
+
+}
